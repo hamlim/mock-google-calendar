0432e03b26d5f4d4bf1bb8e4859e1aec0ff02b99
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-10-03 09:21:12 -0400

    Mostly working

--- apps/google-calendar/app/page.tsx
+++ apps/google-calendar/app/page.tsx
@@ -29,58 +29,256 @@ function Input({ name, type, ...props }: { name: string; type: string }) {
   );
 }
 
-function Textarea({ name }: { name: string }) {
-  return <textarea className="border rounded p-3" name={name} />;
+function Textarea({ name, ...props }: { name: string }) {
+  return <textarea className="border rounded p-3" name={name} {...props} />;
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "set-start-time": {
+      let errors = state.errors;
+      if (errors && errors.startTime) {
+        delete errors.startTime;
+      }
+      return {
+        ...state,
+        errors,
+        form: {
+          ...state.form,
+          startTime: action.payload,
+        },
+      };
+    }
+    case "set-title": {
+      let errors = state.errors;
+      if (errors && errors.title) {
+        delete errors.title;
+      }
+      return {
+        ...state,
+        errors,
+        form: {
+          ...state.form,
+          title: action.payload,
+        },
+      };
+    }
+    case "set-description": {
+      return {
+        ...state,
+        form: {
+          ...state.form,
+          description: action.payload,
+        },
+      };
+    }
+    case "set-end-time": {
+      let errors = state.errors;
+      if (errors && errors.endTime) {
+        delete errors.endTime;
+      }
+      return {
+        ...state,
+        errors,
+        form: {
+          ...state.form,
+          endTime: action.payload,
+        },
+      };
+    }
+    case "create": {
+      if (!state.form.startTime || !state.form.endTime || !state.form.title) {
+        return {
+          ...state,
+          errors: {
+            startTime: !state.form.startTime,
+            endTime: !state.form.endTime,
+            title: !state.form.title,
+          },
+        };
+      }
+
+      return {
+        ...state,
+        form: {
+          startTime: "",
+          endTime: "",
+          title: "",
+          description: "",
+        },
+        events: [
+          ...state.events,
+          {
+            title: state.form.title,
+            startTime: state.form.startTime,
+            endTime: state.form.endTime,
+            description: state.form.description,
+          },
+        ],
+      };
+    }
+  }
+}
+
+function startToOffset(startTime: string): number {
+  let hour = startTime.split(":")[0];
+  hour = Number.parseInt(hour, 10);
+
+  return hour * 200;
+}
+
+function computeHeight(evt) {
+  let startTime = Number.parseInt(evt.startTime.split(":")[0]);
+  let endTime = Number.parseInt(evt.endTime.split(":")[0]);
+
+  return (endTime - startTime) * 200;
 }
 
 export default function Home() {
+  let [state, dispatch] = useReducer(reducer, {
+    form: {
+      startTime: "",
+      endTime: "",
+      title: "",
+      description: "",
+    },
+    events: [],
+    errors: {},
+  });
   return (
     <main className="p-4 min-h-screen grid grid-cols-2  gap-3">
-      <div className="grid grid-rows-[repeat(24,1fr)]">
-        {Array.from({ length: 24 }).map((_, index) => (
-          <div
-            className="border border-t-0 first:border-t"
-            key={index.toString()}
-          >
-            {index}
-          </div>
-        ))}
+      <div className="relative">
+        <div className="day">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <button
+              type="button"
+              onClick={() => {
+                dispatch({
+                  type: "set-start-time",
+                  payload: `${`${index}`.padStart(2, "0")}:00`,
+                });
+              }}
+              className="hour"
+              key={index.toString()}
+            >
+              {index}
+            </button>
+          ))}
+        </div>
+        <div className="events">
+          {state.events.map((evt) => (
+            <div
+              key={evt.title + evt.startTime + evt.endTime + evt.description}
+              className="event"
+              style={{
+                "--top-offset": startToOffset(evt.startTime),
+                "--height": computeHeight(evt),
+              }}
+            >
+              {evt.title}
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target as HTMLFormElement);
-            const data = Object.fromEntries(formData.entries());
-            console.log(data);
-          }}
-          className="flex max-w-md m-auto flex-col gap-3"
-        >
+      <div className="fixed right-0 top-0 w-1/2">
+        <div className="flex max-w-md m-auto flex-col gap-3">
           <Label>
             Title
-            <Input name="title" type="text" />
+            {state.errors.title ? (
+              <>
+                <br />
+                <span className="text-red-600">Missing value</span>
+              </>
+            ) : null}
+            <Input
+              name="title"
+              value={state.form.title}
+              required
+              onChange={(e) => {
+                let val = e.target.value;
+                dispatch({
+                  type: "set-title",
+                  payload: val,
+                });
+              }}
+              type="text"
+            />
           </Label>
           <Label>
             Start Time
-            <Input step={3600} name="start-time" type="time" />
+            {state.errors.startTime ? (
+              <>
+                <br />
+                <span className="text-red-600">Missing value</span>
+              </>
+            ) : null}
+            <Input
+              step={3600}
+              name="start-time"
+              required
+              type="time"
+              value={state.form.startTime}
+              onChange={(e) => {
+                let val = e.target.value;
+                dispatch({
+                  type: "set-start-time",
+                  payload: val,
+                });
+              }}
+            />
           </Label>
           <Label>
             End Time
-            <Input step={3600} name="end-time" type="time" />
+            {state.errors.endTime ? (
+              <>
+                <br />
+                <span className="text-red-600">Missing value</span>
+              </>
+            ) : null}
+            <Input
+              step={3600}
+              name="end-time"
+              required
+              type="time"
+              value={state.form.endTime}
+              onChange={(e) => {
+                let val = e.target.value;
+                dispatch({
+                  type: "set-end-time",
+                  payload: val,
+                });
+              }}
+            />
           </Label>
           <Label>
             Description
-            <Textarea name="description" />
+            <Textarea
+              name="description"
+              value={state.form.description}
+              onChange={(e) => {
+                let val = e.target.value;
+                dispatch({
+                  type: "set-description",
+                  payload: val,
+                });
+              }}
+            />
           </Label>
           <div className="flex justify-end">
             <button
               className="rounded p-3 text-white bg-green-700"
-              type="submit"
+              type="button"
+              onClick={() => {
+                dispatch({
+                  type: "create",
+                });
+              }}
             >
               Create event
             </button>
           </div>
-        </form>
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
       </div>
     </main>
   );
